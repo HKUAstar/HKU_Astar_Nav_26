caf977a8902cbcfa57095989627cacc6af5f4681
fffcr <u3648020@connect.hku.hk>
2026-03-13 21:36:15 +0800

    Initial commit

---
+++ ws_livox/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sentry_train_test/AstarTraining/ws_livox/src
+# Build directory: /home/sentry_train_test/AstarTraining/ws_livox/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("livox_ros_driver2")
---
+++ ws_livox/devel/share/livox_ros_driver2/cmake/livox_ros_driver2-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(livox_ros_driver2_MESSAGE_FILES "/home/sentry_train_test/AstarTraining/ws_livox/src/livox_ros_driver2/msg/CustomPoint.msg;/home/sentry_train_test/AstarTraining/ws_livox/src/livox_ros_driver2/msg/CustomMsg.msg")
+set(livox_ros_driver2_SERVICE_FILES "")
